bfd532ab393db0a66140d6b5438e175b8598f4f5
Wadah Adlan <wadahadlan@gmail.com>
2022-10-06 19:39:35 -0400

    added models

--- authentication-service/data/models.go
+++ authentication-service/data/models.go
@@ -1,1 +1,39 @@
 package data
+
+import (
+	"database/sql"
+	"time"
+)
+
+const dbTimeout = time.Second * 3
+
+var db *sql.DB
+
+// New is the function used to create an instance of the daa package. It returns the type
+// Model, which embeds all the types we want to be availible to our application.
+func New(dbPool *sql.DB) Models {
+	db = dbPool
+
+	return Models{
+		User: User{},
+	}
+}
+
+// Models is the type for this package. Note that any model tha is included as a membner in the type is availble
+// to us throughout the application, anywhere that the app
+// variable is used, provided that the model is also added in the New function.
+type Models struct {
+	User User
+}
+
+// User is the structure which holds one user from the database.
+type User struct {
+	ID        int       `json:"id"`
+	Email     string    `json:"email"`
+	FirstName string    `json:"first_name,omitempty"`
+	LastName  string    `json:"last_name,omitempty"`
+	Password  string    `json:"-"`
+	Active    int       `json:"active"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
