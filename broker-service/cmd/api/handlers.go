146d060bab74537abb8d89dc809a6e5d3e1dab99
Wadah Adlan <wadahadlan@gmail.com>
2022-10-07 19:56:32 -0400

    running

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -7,16 +7,14 @@ import (
 	"net/http"
 )
 
-// Create a predicatble JSON Format to adhere to
-
 type RequestPayload struct {
 	Action string      `json:"action"`
-	Auth   AuthPayload `json:"auth, omitempty"`
+	Auth   AuthPayload `json:"auth,omitempty"`
 }
 
 type AuthPayload struct {
-	Email    string `json:"string"`
-	Password string `json:"string"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -48,13 +46,13 @@ func (app *Config) HandleSubmissions(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// authenticate calls the authentication microservice and sends back the appropriate response
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
-	// create some json to send to auth microservce
+	// create some json we'll send to the auth microservice
 	jsonData, _ := json.MarshalIndent(a, "", "\t")
 
 	// call the service
 	request, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
-
 	if err != nil {
 		app.errorJSON(w, err)
 		return
@@ -64,12 +62,11 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	response, err := client.Do(request)
 	if err != nil {
 		app.errorJSON(w, err)
+		return
 	}
 	defer response.Body.Close()
-	// make sure we get nack the correct status code
-	// be sure to check for unauthorized status
-	// and check fro status accepted
-	// otherwise youd have some other successful unauthorized code
+
+	// make sure we get back the correct status code
 	if response.StatusCode == http.StatusUnauthorized {
 		app.errorJSON(w, errors.New("invalid credentials"))
 		return
@@ -78,13 +75,13 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 		return
 	}
 
-	// create a variable we'll read response.Bpdy into
+	// create a variable we'll read response.Body into
 	var jsonFromService JsonResponse
-	// decode json from auth service
 
+	// decode the json from the auth service
 	err = json.NewDecoder(response.Body).Decode(&jsonFromService)
 	if err != nil {
-		app.errorJSON(w, errors.New("invalid credentials"))
+		app.errorJSON(w, err)
 		return
 	}
 
