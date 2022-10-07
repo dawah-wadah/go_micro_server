697ceb8c559010f7a9bfe1f2d66ea1c10afbdcfd
Wadah Adlan <wadahadlan@gmail.com>
2022-10-06 20:42:05 -0400

    set up BE for auth

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "database/sql"
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 const webPort = "80"
 
@@ -10,5 +16,21 @@ type Config struct {
 }
 
 func main() {
+	log.Println("Starting Authenticating Service")
 
+	// TODO Connect to DB
+
+	// Set up config
+
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 }
