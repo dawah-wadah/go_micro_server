a815f7c503a9156c9a65be5a26eafed71389e3fc
Wadah Adlan <wadahadlan@gmail.com>
2022-10-14 02:36:13 -0400

    add consumer and event file

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,8 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
 func main() {
 	// try to connect to RabbitMQ
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+	log.Println("Connected to RabbitMQ!")
 	// start listening for messages
 	// create a consumer
 	// watch the queue and consume events
+}
+
+func connect() (*amqp.Connection, error) {
+	// Note: RabbitMQ is slow to start but once it starts its rock solid
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// dont continue until rabbit is ready
+
+	for {
+
+		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready: %d", counts)
+			counts++
+		} else {
+			connection = c
+			break
+		}
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("Backing Off")
+		time.Sleep(backOff)
+		continue
+	}
+	return connection, nil
 }
