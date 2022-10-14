69631c86911c4aa9298439d3b64612e030c2dff9
Wadah Adlan <wadahadlan@gmail.com>
2022-10-13 20:48:58 -0400

    added beginning of logger db methods

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -2,35 +2,36 @@ package main
 
 import (
 	"context"
-	"log"
 	"time"
 )
 
 const (
 	webPort  = "80"
-	rpcPort  = "5001"
+	rpcPort  = 5001
 	mongoURL = "mongodb://mongo:27017"
-	grpcPort = "5001"
+	grpcPort = "50001"
 )
 
 var client *mongo.Client
 
-type Config struct{}
+type Config struct {
+}
 
 func main() {
-	// connect to MongoDB
+	// Install mongo drivers
+	// connect to Mongo
 	mongoClient, err := connectToMongo()
 	if err != nil {
 		log.panic(err)
 	}
 	client = mongoClient
 
-	// create a context inorder to disconnect from MongoDB
+	// create a context inorder to disconnect
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
+		if err := client.Disconnect(ctx); err != nil {
 			log.panic(err)
 		}
 	}()
@@ -38,18 +39,18 @@ func main() {
 
 func connectToMongo() (*mongo.Client, error) {
 	// create the connection options
+
 	clientOptions := options.Client().ApplyURI(mongoURL)
-	clientOptions.SetAuth(options.Credential{
+	clientOptions.SetAuth(options.Credentials{
 		Username: "admin",
 		Password: "password",
 	})
-	// connect
-	c, err := mongo.Connect(context.TODO(), clientOptions)
 
+	// connect
+	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		log.Println("Error connecting:", err)
-		return nil, err
+		log.PrintLn("Error Conencting:", err)
 	}
 
-	return c, nil
+	return client, err
 }
--- logger-service/data/models.go
+++ logger-service/data/models.go
@@ -1,17 +1,18 @@
 package data
 
 import (
-	"encoding/base64"
-	"log"
+	"context"
+	"time"
 )
 
 var client *mongo.Client
 
 func New(mongo *mongo.Client) Models {
-	cleint = mongo
-	return Models {
-		LogEntry: LogEntry{}
+	client = mongo
+	return Models{
+		LogEntry: LogEntry{},
 	}
+
 }
 
 type Models struct {
@@ -19,24 +20,27 @@ type Models struct {
 }
 
 type LogEntry struct {
-	ID string `bson:"_id,omitempty" json:"id,omitempty"`
-	Name string `bson:"name,omitempty" json:"name"`
-	Data string `bson:"data" json:"data"`
+	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
+	Name      string    `bson:"name" json:"name"`
+	Data      string    `bson:"data" json:"data"`
 	CreatedAt time.Time `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
 }
 
-func (l *logEntry) Insert(entry LogEntry) error {
-	collection := client.Database("logs").Collection("logs").
+// this is the `receiver` syntax, meaning this function will make use of this context
+func (l *LogEntry) Insert(entry LogEntry) (error error) {
+	// declare a collection variable. can think of them as tables
+	collection := client.Database("logs").Collection("logs")
 
+	// only differnce is we use insert one
 	_, err := collection.InsertOne(context.TODO(), LogEntry{
-		ID: entry.ID,
-        Name: entry.Name,
-        Data: entry.Data,
-        CreatedAt: time.Now(),
-        UpdatedAt:time.Now(),})
+		Name:      entry.Name,
+		Data:      entry.Data,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
 	if err != nil {
-		log.Println("Error inserting into logs", err)
+		log.PrintLn("Error inserting log entry into logs: ", err)
 		return err
 	}
 
@@ -44,32 +48,37 @@ func (l *logEntry) Insert(entry LogEntry) error {
 }
 
 func (l *LogEntry) All() ([]*LogEntry, error) {
-	ctx, cancel := context.WithTimeout(context.Background(),15 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	collection := client.Database("logs").Collection("logs")
 
 	opts := options.Find()
 	opts.SetSort(bson.D{{"created_at", -1}})
+
 	cursor, err := collection.Find(context.TODO(), bson.D{}, opts)
 	if err!= nil {
-        log.Println("Error finding logs", err)
+        log.PrintLn("Finding all docs error:", err)
 		return nil, err
-	}
-	defer cursor.Close(ctx)
+    }
+
+	defer cursor.Close()
 
 	var logs []*LogEntry
+
 	for cursor.Next(ctx) {
 		var item LogEntry
 
-		err := cursor.Decode(&item)
+		err = cursor.Decode(&item)
         if err!= nil {
-			log.Println("Error decoding log into slice", err)
+			log.PrintLn("Error decoding log into slice:", err)
             return nil, err
-		} else {
-			logs = append(logs, &item)
-		}
+	} else {
+		logs = append(logs, &item)
 	}
 
 	return logs, nil
 }
+
+// get record by id
+// delete collection
