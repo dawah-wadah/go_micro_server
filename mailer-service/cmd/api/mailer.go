29a839ab043d0061c597c629bd229b3766daf656
Wadah Adlan <wadahadlan@gmail.com>
2022-10-14 00:22:40 -0400

    email cleint handler and routes built

---
+++ mailer-service/cmd/api/mailer.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"bytes"
+	"html/template"
+	"time"
+
+	"github.com/vanng822/go-premailer/premailer"
+	mail "github.com/xhit/go-simple-mail/v2"
+)
+
+// set up instance of mailer with appropriate config
+type Mail struct {
+	Domain      string
+	Host        string
+	Port        int
+	Username    string
+	Password    string
+	Encryption  string
+	FromAddress string
+	FromName    string
+}
+
+type Message struct {
+	From       string
+	FromName   string
+	To         string
+	Subject    string
+	Attachment []string
+	Data       any
+	DataMap    map[string]any
+}
+
+// funct to send email
+func (m *Mail) SendSMTPMessage(msg Message) error {
+	if msg.From == "" {
+		// use default
+		msg.From = m.FromAddress
+	}
+	if msg.FromName == "" {
+		// use default
+		msg.FromName = m.FromName
+	}
+
+	data := map[string]any{
+		"message": msg.Data,
+	}
+
+	msg.DataMap = data
+
+	// html formated message
+	formattedMessage, err := m.buildHTMLMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	plainMessage, err := m.buildPlainTextMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	server := mail.NewSMTPCLient()
+	// describe server
+	server.Host = m.Host
+	server.Port = m.Port
+	server.Username = m.Username
+	server.Password = m.Password
+	// set the encrption programtically so when we go to prod
+	// we can switch from mailhog to our production mail server
+	server.Encryption = m.getEncryption(m.Encryption)
+	server.KeepAlive = false
+	server.ConnectTimeout = 10 * time.Second
+	server.SendTimeout = 10 * time.Second
+
+	// connect client and connect to thst host
+	smtpClient, err := server.Connect()
+	if err != nil {
+		return err
+	}
+
+	email := mail.NewMSG()
+	email.SetFrom(msg.From).
+		AddTo(msg.To).
+		SetSubject(msg.Subject)
+	email.SetBody(mail.TextPlain, plainMessage)
+	email.AddAlternative(mail.TextHTML, formattedMessage)
+
+	if len(msg.Attachment) > 0 {
+		for _, x := range msg.Attachment {
+			email.AddAttachment(x)
+		}
+	}
+	// send email
+	err = email.Send(smtpClient)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *Mail) buildHTMLMessage(msg Message) (string, error) {
+	// declare a variable for template to render
+	templateToRender := "./templates/mail.html.gohtml"
+
+	// we need to have some value in there `email-template`
+	t, err := template.New("email-html").ParseFiles(templateToRender)
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	// `body` refers to a section of the template that will be keyed under `body`
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", err
+	}
+
+	formattedMessage := tpl.String()
+	// now add in-line css
+	formattedMessage, err = m.inLineCSS(formattedMessage)
+	if err != nil {
+		return "", err
+	}
+	return formattedMessage, nil
+}
+
+func (m *Mail) buildPlainTextMessage(msg Message) (string, error) {
+	// declare a variable for template to render
+	templateToRender := "./templates/mail.plain.gohtml"
+
+	// we need to have some value in there `email-template`
+	t, err := template.New("email-plain").ParseFiles(templateToRender)
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	// `body` refers to a section of the template that will be keyed under `body`
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", err
+	}
+
+	plainMessage := tpl.String()
+	return plainMessage, nil
+}
+
+func (m *Mail) inLineCSS(s string) (string, error) {
+	options := premailer.Options{
+		RemoveClasses:     false,
+		CssToAttributes:   false,
+		KeepBangImportant: true,
+	}
+	prem, err := premailer.NewPremailerFromString(s, &options)
+	if err != nil {
+		return "", err
+	}
+	html, err := prem.Transform()
+	if err != nil {
+		return "", err
+	}
+	return html, nil
+}
+
+func (m *Mail) getEncryption(encryption string) string {
+	switch encryption {
+	case "tls":
+		return mail.EncryptionSTARTTLS
+	case "ssl":
+		return mail.EncryptionSSL
+	case "none":
+		return mail.EncryptionNONE
+	default:
+		return mail.EncryptionSTARTTLS
+	}
+}
