37128c51d942cfd368b75ee3228f73620cb95782
Wadah Adlan <wadahadlan@gmail.com>
2022-10-13 22:30:20 -0400

    log in logging

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -38,6 +38,13 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 		app.errorJSON(w, err)
 	}
 
+	// log authentication
+	err = app.logRequest(w, "authentication", fmt.Sprintf("%s logged in", user.Email))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
 	payload := JsonResponse{
 		Error:   false,
 		Message: fmt.Sprintf("Logged in user %s", user.Email),
@@ -63,6 +70,30 @@ func (app *Config) logRequest(name string, data string) error {
 		return err
 	}
 
+	client := &http.Client{}
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (app *Config) logRequest(name, data string) error {
+	var entry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+
+	entry.Name = name
+	entry.Data = data
+
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+	logServiceURL := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
 	client := &http.Client{}
 	_, err = client.Do(request)
 	if err != nil {
